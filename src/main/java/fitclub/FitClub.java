d70adaa7d060c5bcde00d290d3ffab5c0fba5c8e
Camilo Duran <camiloandresduran@unicesar.edu.co>
2026-05-30 10:14:41 -0500

    agregar el MainFrame y ReportePanel a la capa view

--- src/main/java/fitclub/FitClub.java
+++ src/main/java/fitclub/FitClub.java
@@ -1,4 +1,10 @@
 package fitclub;
 
+import fitclub.view.MainFrame;
+import javax.swing.*;
+
 public class FitClub {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(MainFrame::new);
+    }
 }
