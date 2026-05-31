17dbcef40d9fd3ba92e1df38c95a0c27820a1566
Camilo Duran <camiloandresduran@unicesar.edu.co>
2026-05-31 18:28:19 -0500

    feat: agregar funcionalidad cancelarMembresia

--- src/main/java/fitclub/dao/IMembresiaDAO.java
+++ src/main/java/fitclub/dao/IMembresiaDAO.java
@@ -6,7 +6,8 @@ import java.util.List;
 public interface IMembresiaDAO {
     void insertar(Membresia membresia, String clienteCedula);
     void actualizar(Membresia membresia);
-    void eliminar(int idMembresia);
+    void cancelar(int idMembresia);
     Membresia buscarPorId(int idMembresia);
     List<Membresia> listarPorCliente(String clienteCedula);
+    List<Membresia> listarActivasPorCliente(String clienteCedula);
 }
