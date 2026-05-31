47724411a2a9b0bd7807e54dedfa29b1cf90fff7
Camilo Duran <camiloandresduran@unicesar.edu.co>
2026-05-31 17:46:28 -0500

    feat: agregar desactivarCliente() en la interface IClienteDAO

--- src/main/java/fitclub/dao/IClienteDAO.java
+++ src/main/java/fitclub/dao/IClienteDAO.java
@@ -6,7 +6,8 @@ import java.util.List;
 public interface IClienteDAO {
     void insertar(Cliente cliente);
     void actualizar(Cliente cliente);
-    void eliminar(String cedula);
+    void desactivar(String cedula);
     Cliente buscarPorCedula(String cedula);
     List<Cliente> listarTodos();
+    List<Cliente> listarActivos();
 }
